e245cbdcb63cb9fad04dfff64fb8c7e50cf9051a
ThePhantomAngel <bouzbiproto@gmail.com>
2015-11-16 01:15:09 +0200

    newsite

---
+++ newsite/scripty.js
@@ -0,0 +1,15 @@
+function triggerbox() {
+document.getElementById('lightbox').style.opacity='1';
+document.getElementById('lightbox').style.visibility='visible';
+document.getElementById('thex').style.opacity='1';
+document.getElementById('fade').style.opacity='0.8';
+document.getElementById('fade').style.display='block';
+}
+
+function closelightbox() {
+document.getElementById('lightbox').style.opacity='0';
+document.getElementById('lightbox').style.visibility='hidden';
+document.getElementById('thex').style.opacity='0';
+document.getElementById('fade').style.opacity='0';
+document.getElementById('fade').style.display='none';
+}
