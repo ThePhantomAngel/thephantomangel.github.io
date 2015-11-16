46a8daa4412a58b95d5f7c4197aa5a579e5be47b
ThePhantomAngel <bouzbiproto@gmail.com>
2015-11-17 01:12:31 +0200

    newsite

--- newsite/scripty.js
+++ newsite/scripty.js
@@ -2,6 +2,7 @@ function triggerbox() {
 document.getElementById('lightbox').style.opacity='1';
 document.getElementById('lightbox').style.visibility='visible';
 document.getElementById('thex').style.opacity='1';
+document.getElementById('thex').style.visibility='visible';
 document.getElementById('fade').style.opacity='0.8';
 document.getElementById('fade').style.display='block';
 }
@@ -10,6 +11,7 @@ function closelightbox() {
 document.getElementById('lightbox').style.opacity='0';
 document.getElementById('lightbox').style.visibility='hidden';
 document.getElementById('thex').style.opacity='0';
+document.getElementById('thex').style.visibility='hidden';
 document.getElementById('fade').style.opacity='0';
 document.getElementById('fade').style.display='none';
 }
