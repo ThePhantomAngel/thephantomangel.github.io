ba2fbe392471a91206e3bf2d2c79cf936a3f43cb
protocrash <aproto.ecom@gmail.com>
2016-02-15 19:53:45 +0200

    fix

--- scripty.js
+++ scripty.js
@@ -5,7 +5,6 @@ document.getElementById('thex').style.opacity='1';
 document.getElementById('thex').style.visibility='visible';
 document.getElementById('fade').style.opacity='0.8';
 document.getElementById('fade').style.display='block';
-console.log("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
 }
 
 function closelightbox() {
@@ -17,4 +16,23 @@ document.getElementById('fade').style.opacity='0';
 document.getElementById('fade').style.display='none';
 }
 
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var updateCurrencies = function() {
+	console.log("Welcome to our visitors from "+geoplugin_city()+", "+geoplugin_countryName()); 
+};
