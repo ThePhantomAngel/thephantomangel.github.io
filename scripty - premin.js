f2225b29cb968e504e27032d418ba6cc3e519358
protocrash <aproto.ecom@gmail.com>
2016-03-24 17:13:38 +0200

    Optimisations and minificaitons

---
+++ scripty - premin.js
@@ -0,0 +1,69 @@
+function triggerbox() {
+document.getElementById('lightbox').style.opacity='1';
+document.getElementById('lightbox').style.visibility='visible';
+document.getElementById('thex').style.opacity='1';
+document.getElementById('thex').style.visibility='visible';
+document.getElementById('fade').style.opacity='0.8';
+document.getElementById('fade').style.display='block';
+}
+
+function closelightbox() {
+document.getElementById('lightbox').style.opacity='0';
+document.getElementById('lightbox').style.visibility='hidden';
+document.getElementById('thex').style.opacity='0';
+document.getElementById('thex').style.visibility='hidden';
+document.getElementById('fade').style.opacity='0';
+document.getElementById('fade').style.display='none';
+}
+
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var updateCurrencies = function() {
+	
+	if (geoplugin_currencyCode()=='EUR') {
+		//Euros
+		var inspireprice = '€43.99';
+		var phantom3price = '€35.99';
+		var phantom23price = '€37.99';
+		document.getElementById('inspireprice').innerHTML=inspireprice;
+		document.getElementById('phantom3price').innerHTML=phantom3price;
+		document.getElementById('phantom23price').innerHTML=phantom23price;
+		document.getElementById('phantom3priceintext').innerHTML=phantom3price;
+		document.getElementById('inspirepriceintext').innerHTML=inspireprice;
+		document.getElementById('postageprice').innerHTML='€4.5';
+		document.getElementById('paypalinspirebuttonid').value='AFAMEHZNH5UXW';
+		document.getElementById('paypalP3buttonid').value='QMYUGG2G8Y3MU';
+		document.getElementById('paypalP23buttonid').value='GAUQBSFK2D2HE';
+	}
+	if (geoplugin_currencyCode()=='GBP') {
+		//GBP
+		var inspireprice = '£33.99';
+		var phantom3price = '£27.99';
+		var phantom23price = '£29.99';
+		document.getElementById('inspireprice').innerHTML=inspireprice;
+		document.getElementById('phantom3price').innerHTML=phantom3price;
+		document.getElementById('phantom23price').innerHTML=phantom23price;
+		document.getElementById('phantom3priceintext').innerHTML=phantom3price;
+		document.getElementById('inspirepriceintext').innerHTML=inspireprice;
+		document.getElementById('postageprice').innerHTML='£3.99';
+		document.getElementById('paypalinspirebuttonid').value='NWKHACP87F3YQ';
+		document.getElementById('paypalP3buttonid').value='7DVAFHP7DJLW4';
+		document.getElementById('paypalP23buttonid').value='6MBEL3D6MEAV2';
+	}
+};
+
