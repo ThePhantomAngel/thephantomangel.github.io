3f833a328a56c2b6042ef05210b2fc7076378a00
protocrash <aproto.ecom@gmail.com>
2016-08-01 17:11:28 +0300

    Added facebook tracking

--- scripty.js
+++ scripty.js
@@ -1,17 +1,8 @@
-function triggerbox() {
-document.getElementById('lightbox').style.opacity='1';
-document.getElementById('lightbox').style.visibility='visible';
-document.getElementById('thex').style.opacity='1';
-document.getElementById('thex').style.visibility='visible';
-document.getElementById('fade').style.opacity='0.8';
-document.getElementById('fade').style.display='block';
+function trackBuyNow()
+{
+fbq('track', 'Lead');
 }
-
-function closelightbox() {
-document.getElementById('lightbox').style.opacity='0';
-document.getElementById('lightbox').style.visibility='hidden';
-document.getElementById('thex').style.opacity='0';
-document.getElementById('thex').style.visibility='hidden';
-document.getElementById('fade').style.opacity='0';
-document.getElementById('fade').style.display='none';
+function trackPaypalClick()
+{
+fbq('track', 'AddToCart');
 }
