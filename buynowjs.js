af654162b0e5664398f0a43455bffa4a2e7e32aa
protocrash <aproto.ecom@gmail.com>
2018-10-26 00:14:30 +0300

    Experimenting...

---
+++ buynowjs.js
@@ -0,0 +1,6 @@
+function startedTyping() {
+	if (document.getElementById('combochoice').value!="") {
+		console.log('hasvalue');
+		document.getElementById('comboform').action = 'https://wt-b809e76a147c10b17bc1c6ea6f6d8d6c-0.sandbox.auth0-extend.com/thisistheone/payment?description=' + encodeURIComponent(document.getElementById('combochoice').value);
+	}
+}
