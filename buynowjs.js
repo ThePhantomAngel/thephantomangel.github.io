b4869a396e4da207558c444f64848c388780bbdf
protocrash <aproto.ecom@gmail.com>
2018-10-27 18:12:17 +0300

    trying some analytics

--- buynowjs.js
+++ buynowjs.js
@@ -4,3 +4,7 @@ function startedTyping() {
 		document.getElementById('comboform').action = 'https://wt-b809e76a147c10b17bc1c6ea6f6d8d6c-0.sandbox.auth0-extend.com/thisistheone/payment?description=' + encodeURIComponent(document.getElementById('combochoice').value);
 	}
 }
+
+function trythis() {
+	console.log("fsdfdsf");
+}
