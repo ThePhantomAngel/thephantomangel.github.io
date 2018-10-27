5fa5c71ab302ab1c71e5f47634f7ce80b655b575
protocrash <aproto.ecom@gmail.com>
2018-10-27 18:34:22 +0300

    Spring cleaning

--- scripty.js
+++ scripty.js
@@ -2,7 +2,16 @@ function trackBuyNow()
 {
 fbq('track', 'Lead');
 }
+
 function trackPaypalClick()
 {
-ga('send', 'event', 'Paypal', 'Paypal', 'Paypal');
+	ga('send', 'event', 'Paypal', 'Paypal', 'Paypal');
 }
+
+function startedTyping() {
+	if (document.getElementById('combochoice').value!="") {
+		console.log('hasvalue');
+		document.getElementById('comboform').action = 'https://wt-b809e76a147c10b17bc1c6ea6f6d8d6c-0.sandbox.auth0-extend.com/thisistheone/payment?description=' + encodeURIComponent(document.getElementById('combochoice').value);
+	}
+}
+
